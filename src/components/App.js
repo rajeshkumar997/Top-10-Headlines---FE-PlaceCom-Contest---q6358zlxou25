28866b2c8f47cad588a0cc756b23f6bbf0c4537e
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-04-01 20:27:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
 const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
   const [loading, setLoading] = useState(false);
-  
+
   useEffect(() => {
     setLoading(true);
-    fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY]&max=10&lang=en`)
-    .then((res) => res.json())
-    .then((data) => {
-      setNewsData(data.articles);
-      setLoading(false);
-  })
-    .catch((err) => console.log(err));
-  },[category]);
-  
+    fetch(
+      `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY]&max=10&lang=en`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setNewsData(data.articles);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [category]);
+
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
   };
-  
+
   return (
     <div id="main">
-      <h1 className='heading'>Top 10 {category} news.</h1>
+      <h1 className="heading">Top 10 {category} news</h1>
       <select value={category} onChange={handleCategoryChange}>
         <option value="general">General</option>
         <option value="business">Business</option>
@@ -33,27 +35,27 @@ const App = () => {
         <option value="entertainment">Entertainment</option>
         <option value="science">Science</option>
       </select>
-{loading && 
-      <p className='loader'>Loading...</p>
- {!loading && (
-      <ol>\
-   {newsData.map((news,index) => (
-        <li key={index}>
-          <img className='news-img' src={news.image} alt=""/>
-          <section className='new-title-content-author'>
-            <h3 className='news-title'>{news.title}</h3>
-            <section className='new-content-author'>
-              <p className='news-description'>{news.description}</p>
-              <p className='news-source'><strong>Source:</strong>{news.source.name}</p>
-            </section>
-          </section>
-        </li>
- ))}
-      </ol>
-)}
+      {loading && <p className="loader">Loading...</p>}
+      {!loading && (
+        <ol>\
+          {newsData.map((news, index) => (
+            <li key={index}>
+              <img className="news-img" src={news.image} alt="" />
+              <section className="new-title-content-author">
+                <h3 className="news-title">{news.title}</h3>
+                <section className="new-content-author">
+                  <p className="news-description">{news.description}</p>
+                  <p className="news-source">
+                    <strong>Source:</strong> {news.source.name}
+                  </p>
+                </section>
+              </section>
+            </li>
+          ))}
+        </ol>
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
